02b529742f9a1c76c49e13b3418d09a001ecafb3
nguyentienloc653 <locnguyentien653@gmail.com>
2024-11-21 01:19:51 +0700

    up load bài tập ss2-1

---
+++ bài 1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+	// kieu so nguyen int,short,long
+	int x = 10;
+	// kieu so thuc float,double
+	float pi = 3.14;
+	// kieu ky tu char
+	char kytu = 'A';
+	// kieu logic Dùng de luu giá tri true hoac false. (thuong dùng voi int trong C)
+	bool ladung = true ;
+	return 0 ;
+}
