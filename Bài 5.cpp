d54de3102630a938954488af94a0eb31317d329e
nguyentienloc653 <locnguyentien653@gmail.com>
2024-11-21 01:21:06 +0700

    up load bài tập ss2-5

---
+++ Bài 5.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a = 5;  
+    int b = 4; 
+    int chuVi = 2 * (a + b);  
+    int dienTich = a * b;             
+    printf("Chu vi cua hinh chu nhat la: %d\n", chuVi);
+    printf("Dien tich cua hinh chu nhat la: %d", dienTich);
+    return 0;
+}
