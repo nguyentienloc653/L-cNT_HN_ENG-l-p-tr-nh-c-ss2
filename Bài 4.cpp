31023f36eb69cb6c67626bd3e734e8633edc4e01
nguyentienloc653 <locnguyentien653@gmail.com>
2024-11-21 01:20:47 +0700

    up load bài tập ss2-4

---
+++ Bài 4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a = 4; 
+    int chuVi = 4*a; 
+    int dienTich = a*a; 
+
+    printf("Chu vi cua hinh vuong: %d\n", chuVi);
+    printf("Dien tich cua hinh vuong: %d", dienTich);
+    return 0;
+}
