a5608dc94e37fa4b46cb1f73bdc182ab504e4cf4
nguyentienloc653 <locnguyentien653@gmail.com>
2024-11-21 01:21:27 +0700

    up load bài tập ss2-6

---
+++ Bài 6.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+	float pi = 3.14;
+	float r = 4;
+	float chuVi = 2*pi*r;
+	float dienTich = pi*r*r;
+	printf("Chu vi hinh tron %.2f\n", chuVi);
+	printf("Dien tich hinh tron %.2f\n", dienTich);
+	
+	return 0;
+}
